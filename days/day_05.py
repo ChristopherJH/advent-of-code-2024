b0d0f9611dddb951e5107927c4776bb133198fd6
Christopher Hallett <chrisjameshal@gmail.com>
2024-12-05 21:04:20 +0000

    tidy up

--- days/day_05.py
+++ days/day_05.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+
 def generate_order_dictionary(order_elements: list[list[int]]):
     """Given page ordering rules, generates an object outlining the numbers before and after for each number in the input
     Example output:
@@ -30,32 +31,39 @@ def generate_order_dictionary(order_elements: list[list[int]]):
 
     return order_dict
 
+
 def contains_all_elements(array1, array2):
+    """Returns True if all the elements in array2 appear in array1"""
     return all(element in array1 for element in array2)
 
-def do_page_numbers_obey_rules(order_dict: dict[int, dict[str, list[int]]], page_numbers: list[int]):
+
+def do_page_numbers_obey_rules(
+    order_dict: dict[int, dict[str, list[int]]], page_numbers: list[int]
+):
+    """Returns True if all the page numbers in page_numbers follow the page ordering rules"""
     for i, page_number in enumerate(page_numbers):
         page_rules = order_dict[page_number]
         page_numbers_before = page_numbers.copy()[:i]
-        page_numbers_after = page_numbers.copy()[i+1:]
-        
-        
+        page_numbers_after = page_numbers.copy()[i + 1 :]
 
         if not contains_all_elements(page_rules["before"], page_numbers_before):
             return False
         if not contains_all_elements(page_rules["after"], page_numbers_after):
             return False
-    
+
     return True
 
-def get_middle_sum_of_correctly_ordered_pages(page_ordering_rules: list[list[int]], pages_to_produce: list[list[int]]):
+
+def get_middle_sum_of_correctly_ordered_pages(
+    page_ordering_rules: list[list[int]], pages_to_produce: list[list[int]]
+):
+    """Returns the sum of all the middle pages numbers in the correctly ordered lists of pages_to_produce"""
     order_dict = generate_order_dictionary(page_ordering_rules)
     total = 0
-    
+
     for page_numbers in pages_to_produce:
         if do_page_numbers_obey_rules(order_dict, page_numbers):
             middle_index = len(page_numbers) // 2
             total += page_numbers[middle_index]
-            
+
     return total
-    
